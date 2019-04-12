5459a77564cb9b1ac2d077e5a353976673f5c02a
Paul Griffin <pgriffin@kth.se>
2019-04-12 14:39:39 +0200

    added adsr

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,16 @@ use ui::Ui;
 #[allow(unused_imports)]
 use std::f64::consts::PI;
 
+use std::sync::mpsc::channel;
+
+#[derive(Copy, Clone)]
+enum State{
+    Attack,
+    Decay,
+    Sustain,
+    Release
+}
+
 #[allow(unused_variables)]
 fn main() -> Result<(), Error> {
     let audioengine = audioengine::EngineController::start();
@@ -26,7 +36,21 @@ fn main() -> Result<(), Error> {
 
     let mut time = 0.0;
 
+    // Create channel for communication between UI and Audio thread
+    let (sample_sender, sample_receiver) = channel::<Vec<f64>>();
+    let mut buffer = [0_f64; 100];
+    let mut buffer_index = 0;
+
     let mut current_key = None;
+    let mut frequency: f64 = 440_f64;
+
+    let mut gate = 0_f64;
+    let mut value = 0_f64;
+    let mut attack = 1000_f64;
+    let mut decay = 1000_f64;
+    let mut sustain = 0.5_f64;
+    let mut release = 1000_f64;
+    let mut state = State::Release;
     let synth = move |action: Option<i32>| {
         time += time_per_sample;
         if action != current_key {
@@ -34,14 +58,41 @@ fn main() -> Result<(), Error> {
 
             println!("{:?}", action);
         }
+        let sample: f64;
         if let Some(action) = action {
-            let frequency = 440_f64 * 2_f64.powf((action as f64) / 12_f64);
-            return (time * frequency * 2_f64 * 3.1415_f64).sin();
+            frequency = 440_f64 * 2_f64.powf((action as f64) / 12_f64);
+            gate = 1_f64;
         } else {
-            return 0_f64;
+            gate = 0_f64;
         }
-    };
+        match state {
+            State::Attack => {
+                value += 1_f64 / attack;
+                if value >= 1_f64 { state = State::Decay; }
+                if gate < 0.5_f64 { state = State::Release; }
+            },
+            State::Decay => {
+                value = value - (value - sustain)/decay;
+                if gate < 0.5_f64 { state = State::Release; }
+            },
+            State::Sustain => (),
+            State::Release => {
+                value = value - value / release;
+                if gate > 0.5_f64 { state = State::Attack; }
+            },
+        }
+        value = value.min(1_f64).max(0_f64);
+        sample = (time * frequency * 2_f64 * 3.1415_f64).sin().round() * value;
 
+        buffer[buffer_index] = sample;
+        if buffer_index == buffer.len() - 1 {
+            let _ = sample_sender.send(buffer.to_vec());
+            buffer_index = 0;
+        } else {
+           buffer_index = buffer_index + 1;
+        }
+        return sample
+    };
     audioengine.set_processor_function(Box::new(synth));
 
     let mut window = Ui::new(
@@ -50,7 +101,7 @@ fn main() -> Result<(), Error> {
         audioengine,
         None,
         None,
-        None,
+        Some(sample_receiver),
     );
 
     window.show();
